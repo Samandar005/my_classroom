84e946a94b674c175c149203b519d15f4425a5d3
Samandar <samandarnayimjonov@gmail.com>
2025-02-17 13:10:09 +0200

    added assignment model

--- assignments/models.py
+++ assignments/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.shortcuts import reverse
 
-# Create your models here.
+from courses.models import Course
+from users.base_model import BaseModel
+
+
+class Assignment(BaseModel):
+    name = models.CharField(max_length=200)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, related_name='assignments')
+    description = models.TextField()
+    due_date = models.DateField()
+    max_ball = models.PositiveIntegerField(default=0)
+
+    def __str__(self):
+        return f"{self.name}"
