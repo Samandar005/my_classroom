ba460a682677340bf185d56ab02dc352ec7206c0
Samandar <samandarnayimjonov@gmail.com>
2025-02-17 18:32:20 +0200

    done list course

--- courses/views.py
+++ courses/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from django.views import View
+from .models import Course
 
 
 class CourseListView(View):
+    def get(self, request):
+        courses = Course.objects.all()
+        return render(request, 'courses/courses.html', {'courses': courses})
+
 
