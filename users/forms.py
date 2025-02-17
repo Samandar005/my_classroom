35e2f79d56c82fa76b1f7cc772b3849340abef51
Samandar <samandarnayimjonov@gmail.com>
2025-02-17 13:32:33 +0200

    edit forms.py in users app

--- users/forms.py
+++ users/forms.py
@@ -4,7 +4,7 @@ from .models import User
 class UserForm(forms.ModelForm):
     class Meta:
         model = User
-        fields = ['name', 'email', 'role', 'password1', 'password2']
+        fields = ('name', 'email', 'role', 'password1', 'password2')
         widgets = {
             'name': forms.TextInput(attrs={
                 'class': 'shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline',
