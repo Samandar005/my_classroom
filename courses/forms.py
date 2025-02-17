5f0a438bf2b2bcc5f399b187668fb524a1ec2b40
Samandar <samandarnayimjonov@gmail.com>
2025-02-17 18:27:30 +0200

    edit form

--- courses/forms.py
+++ courses/forms.py
@@ -4,7 +4,7 @@ from .models import Course
 class CourseForm(forms.ModelForm):
     class Meta:
         model = Course
-        fields = ['name', 'code', 'description', 'status', 'teacher']
+        fields = ('name', 'code', 'description', 'status', 'teacher')
 
     def clean_name(self):
         name = self.cleaned_data.get('name')
