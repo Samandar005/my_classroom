48ed944b17025f2cca4ee25705b93acd5279cbff
Samandar <samandarnayimjonov@gmail.com>
2025-02-17 18:35:00 +0200

    impost form

--- courses/views.py
+++ courses/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views import View
 from .models import Course
+from .forms import CourseForm
 
 
 class CourseListView(View):
@@ -8,4 +9,8 @@ class CourseListView(View):
         courses = Course.objects.all()
         return render(request, 'courses/courses.html', {'courses': courses})
 
+class CreateCourseView(View):
+    def get(self, request):
 
+
+    def post(self, request):
