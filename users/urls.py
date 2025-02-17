5b54e8608c94b2d3cefe24857c0ecaae1833c635
Samandar <samandarnayimjonov@gmail.com>
2025-02-17 14:08:07 +0200

    edit urls.py

--- users/urls.py
+++ users/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 from . import  views
 
 app_name='users'
+
 urlpatterns=[
     path('list/', views.UserListView.as_view(), name='list'),
     path('create/', views.UserCreateView.as_view(), name='create'),
     path('update/<int:pk>/', views.UserUpdateView.as_view(), name='update'),
     path('delete/<int:pk>/', views.UserDeleteView.as_view(), name='list'),
-    path('list/', views.UserListView.as_view(), name='list'),
 ]
