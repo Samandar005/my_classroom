d48108aa9f0e7576491693827402edbd30c0bc73
Samandar <samandarnayimjonov@gmail.com>
2025-02-17 18:38:43 +0200

    done url

--- courses/urls.py
+++ courses/urls.py
@@ -5,4 +5,5 @@ from . import views
 app_name='courses'
 urlpatterns=[
     path('list/', views.CourseListView.as_view(), name='list'),
+    path('create/', views.CreateCourseView.as_view(), name='create'),
 ]
