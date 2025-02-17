a33c327968c7f3ed513f6598db6b92c759decac0
Samandar <samandarnayimjonov@gmail.com>
2025-02-17 13:14:05 +0200

    edit models.py

--- assignments/models.py
+++ assignments/models.py
@@ -1,13 +1,12 @@
 from django.db import models
 from django.shortcuts import reverse
-
 from courses.models import Course
 from users.base_model import BaseModel
 
 
 class Assignment(BaseModel):
     name = models.CharField(max_length=200)
-    course = models.ForeignKey(Course, on_delete=models.CASCADE, related_name='assignments')
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, related_name='assignments', null=True, blank=True)
     description = models.TextField()
     due_date = models.DateField()
     max_ball = models.PositiveIntegerField(default=0)
