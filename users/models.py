4821808feec9f3232255daa48cc5d387aaf8fb8d
Samandar <samandarnayimjonov@gmail.com>
2025-02-17 12:04:42 +0200

    user models done

--- users/models.py
+++ users/models.py
@@ -4,3 +4,14 @@ from django.shortcuts import reverse
 
 
 class User(BaseModel):
+    ROLE_CHOICES = [
+        ('p', 'Преподаватель'),
+        ('s', 'Студент'),
+        ('a', 'Администратор'),
+    ]
+
+    name = models.CharField(max_length=200)
+    email = models.EmailField(unique=True)
+    role = models.CharField(max_length=2, choices=ROLE_CHOICES)
+    password1 = models.CharField(max_length=50)
+    password2 = models.CharField(max_length=50)
