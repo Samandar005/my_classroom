08c8ed89766dd98b821135bb56501bf04453d08f
Samandar <samandarnayimjonov@gmail.com>
2025-02-17 13:38:18 +0200

    users viewsga UserCreate logika yozildi

--- users/views.py
+++ users/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from django.views import View
+from .forms import UserForm
 
 
 class HomePageView(View):
@@ -7,8 +8,16 @@ class HomePageView(View):
         return render(request, 'index.html')
 
 class UserCreateView(View):
-    def get(self):
+    def get(self, request):
+        form = UserForm()
+        return render(request, 'user_form.html', {'form': form})
+
+    def post(self, request):
+        form = UserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('users:list')
+        return render(request, 'user_form.html', {'form': form})
 
 
-    def post(self):
 
