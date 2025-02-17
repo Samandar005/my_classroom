b0068ab2ac88f6097c36c54a835e1cf117a31b8d
Samandar <samandarnayimjonov@gmail.com>
2025-02-17 18:38:15 +0200

    added create course

--- courses/views.py
+++ courses/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
 from .models import Course
 from .forms import CourseForm
@@ -11,6 +11,13 @@ class CourseListView(View):
 
 class CreateCourseView(View):
     def get(self, request):
-
+        form = CourseForm()
+        return render(request, 'courses/course-form.html', {'form':form, 'user': None})
 
     def post(self, request):
+        form = CourseForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('courses:list')
+        return render(request, 'courses/course-form.html', {'form': form})
+
