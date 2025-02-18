fbc4df0ef95047b1dba20ecc74e7360507304bd7
Samandar <samandarnayimjonov@gmail.com>
2025-02-18 21:45:34 +0200

    update

--- courses/views.py
+++ courses/views.py
@@ -24,9 +24,17 @@ class CreateCourseView(View):
 
 class UpdateCourseView(View):
     def get(self, request, pk):
-        pass
+        course = get_object_or_404(Course, pk=pk)
+        form = CourseForm(instance=course)
+        return render(request, 'courses/course-form.html', {'form': form})
+
     def post(self, request, pk):
-        pass
+        course = get_object_or_404(Course, pk=pk)
+        form = CourseForm(request.POST, instance=course)
+        if form.is_valid():
+            form.save()
+            return redirect('courses:list')
+        return render(request, 'courses/course-form.html', {'form': form})
 
 class CourseDeleteView(View):
     def get(self, request, pk):
