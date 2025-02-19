b2916e0a52082818f26107f7c3805d348bc39807
Samandar <samandarnayimjonov@gmail.com>
2025-02-19 12:00:02 +0200

    done

--- assignments/urls.py
+++ assignments/urls.py
@@ -9,4 +9,6 @@ urlpatterns=[
     path('create/', views.AssignmentCreateView.as_view(), name='create'),
     path('update/<int:pk>/', views.AssignmentUpdateView.as_view(), name='update'),
     path('delete/<int:pk>/', views.AssignmentDeleteView.as_view(), name='delete'),
+    path('detail/<int:year>/<int:month>/<int:day>/<slug:slug>/', views.AssignmentDetailView.as_view(), name='detail'),
+
 ]
